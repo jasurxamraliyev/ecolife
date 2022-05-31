4cc7061961692f0c66a7fbc1ca2f8d6357beccb2
SanjarbekUsmonov <noobpusk@gmail.com>
2022-05-31 17:21:42 +0500

    ghcybhunuvj,m

--- ecolife/src/pages/IndexPage.vue
+++ ecolife/src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
-<template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+<template lang="">
+  <div>
+
+
+
+
+  </div>
 </template>
-
 <script>
-import { defineComponent } from 'vue'
+export default {
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+}
 </script>
+<style lang="">
+
+</style>
