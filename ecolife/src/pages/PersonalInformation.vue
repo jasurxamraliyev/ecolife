1357e40111db7eb687070fdfc4bcf7c0be07858a
jasurxamraliyev <jasjasurbek4@gmail.com>
2022-06-07 09:26:40 +0300

    responsive

--- ecolife/src/pages/PersonalInformation.vue
+++ ecolife/src/pages/PersonalInformation.vue
@@ -1,24 +1,39 @@
 <template>
   <div>
+    <!-- havbar -->
     <div class="bg-grey w-100pr h-90px"></div>
     <br><br>
+    <!--responsive 1 -->
    <div class="fit row justify-around">
       <div class=" md w-45pr mobile-hide">
         <Persinal/>
       </div>
-      <div class=" md w-40pr q-mt-xl">
+      <div class=" md w-35pr q-mt-xl">
         <Totalpej/>
       </div>
    </div>
-    <div class=" justify-around  row  xs w-100pr ">
-      <div class="w-90pr">
-        <Persinal/>
+   <!--responsive 2 -->
+    <div>
+      <div class=" justify-around  row  xs w-100pr ">
+        <div class="w-90pr">
+          <Persinal/>
+        </div>
+      </div>
+      <div class="row justify-around xs w-100pr q-mt-xl">
+          <Totalpej/>
       </div>
     </div>
-     <div class="row justify-around xs w-100pr q-mt-xl">
+    <!--responsive 3 -->
+    <div class="fit row justify-around" >
+      <div class=" sm w-45pr mobile-hide">
+        <Persinal/>
+      </div>
+      <div class=" sm w-40pr q-mt-xl">
         <Totalpej/>
+      </div>
     </div>
     <br><br>
+    <!-- futer -->
     <div class="h-400px bg-grey w-100pr">
 
     </div>
