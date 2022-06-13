b3222caab64b7c4941020159b16e764d9ea24322
SanjarbekUsmonov <noobpusk@gmail.com>
2022-06-13 16:57:45 +0500

    carusel format

--- ecolife/src/components/CompCarusel.vue
+++ ecolife/src/components/CompCarusel.vue
@@ -7,51 +7,63 @@
       v-model="slide"
       infinite
       :autoplay="autoplay"
-      style="height: 600px;"
+      style="height: 600px"
     >
-      <q-carousel-slide :name="1" >
-        <q-img  src="http://demo.posthemes.com/pos_ecolife/layout2/modules/posslideshows/images/03c53493b66e3228091fb5027e13aa8e78207328_1_2.jpg" >
-          <div  class="w-100pr h-100pr row items-center" style="background: none;">
-            <div class="w-26pr h-33pr q-ml-xl" style="background: none;">
-              <div class="text-black">
-                  COFFEE CREAM & CHOCOLATE
-              </div>
+      <q-carousel-slide :name="1">
+        <q-img
+          src="http://demo.posthemes.com/pos_ecolife/layout2/modules/posslideshows/images/03c53493b66e3228091fb5027e13aa8e78207328_1_2.jpg"
+        >
+          <div
+            class="w-100pr h-100pr row items-center"
+            style="background: none"
+          >
+            <div class="w-26pr h-33pr q-ml-xl" style="background: none">
+              <div class="text-black">COFFEE CREAM & CHOCOLATE</div>
               <div class="text-h4 mt-7px text-black text-weight-bold">
-                  Girella Motta Cacao
-                   Soft Sponge Cake
-              </div >
-               <div class="q-mt-lg">
-                 <q-btn class="btn" unelevated rounded color="secondary" label="SHOP NOW" />
-               </div>
+                Girella Motta Cacao Soft Sponge Cake
+              </div>
+              <div class="q-mt-lg">
+                <q-btn
+                  class="btn"
+                  unelevated
+                  rounded
+                  color="secondary"
+                  label="SHOP NOW"
+                />
+              </div>
             </div>
           </div>
         </q-img>
       </q-carousel-slide>
 
       <q-carousel-slide :name="2">
-        <q-img src="http://demo.posthemes.com/pos_ecolife/layout2/modules/posslideshows/images/081b9c1b802e10a8438349ca084ec5451f955c8e_2_2.jpg">
-          <div  class="w-100pr h-100pr row items-center" style="background: none;">
-            <div class="w-26pr h-33pr q-ml-xl " style="background: none;">
-              <div class="text-black">
-                 CACAO FRUTTI ROSSI
-              </div>
+        <q-img
+          src="http://demo.posthemes.com/pos_ecolife/layout2/modules/posslideshows/images/081b9c1b802e10a8438349ca084ec5451f955c8e_2_2.jpg"
+        >
+          <div
+            class="w-100pr h-100pr row items-center"
+            style="background: none"
+          >
+            <div class="w-26pr h-33pr q-ml-xl" style="background: none">
+              <div class="text-black">CACAO FRUTTI ROSSI</div>
               <div class="text-h4 mt-7px text-black text-weight-bold">
-                  La Girella Motta
-                  Diventa Rosa
-              </div >
-               <div class="q-mt-lg">
-                 <q-btn class="btn" unelevated rounded color="secondary" label="SHOP NOW" />
-               </div>
+                La Girella Motta Diventa Rosa
+              </div>
+              <div class="q-mt-lg">
+                <q-btn
+                  class="btn"
+                  unelevated
+                  rounded
+                  color="secondary"
+                  label="SHOP NOW"
+                />
+              </div>
             </div>
           </div>
         </q-img>
       </q-carousel-slide>
       <template v-slot:control>
-        <q-carousel-control
-          position="bottom-right"
-          :offset="[18, 18]"
-        >
-
+        <q-carousel-control position="bottom-right" :offset="[18, 18]">
         </q-carousel-control>
       </template>
     </q-carousel>
@@ -59,20 +71,20 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
+  setup() {
     return {
       slide: ref(1),
-      autoplay: ref(true)
-    }
-  }
-}
+      autoplay: ref(true),
+    };
+  },
+};
 </script>
 <style scoped>
-.btn{
-  padding: 5px 20px ;
+.btn {
+  padding: 5px 20px;
   font-size: 10px;
 }
 </style>
