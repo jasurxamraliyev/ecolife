6cb6b001bb911798adec7577e931ba3e9793b71e
AliXuB2606 <alixub2006@gmail.com>
2022-06-17 15:48:38 +0500

    main

---
+++ ecolife/src/pages/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page>
+    <div>
+      <card/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Card from '../components/Card.vue'
+export default {
+  components: { Card },
+
+}
+</script>
+
+<style>
+
+</style>
